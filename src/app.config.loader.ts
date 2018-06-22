35e2edbdc43498a4d5076319961d2aa484ef8566
Denis Tatarenko <detat@ciklum.com>
2018-06-22 08:49:28 +0300

    --upd

--- src/app.config.loader.ts
+++ src/app.config.loader.ts
@@ -31,9 +31,12 @@ export class ConfigLoader extends BaseConfigLoader {
          * (maybe ordering(load mongo first) will solve issue))
          **/
         if (process.env.OPENSHIFT_BUILD_NAME) {
-            config.db.host = process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT;
-            config.auth.providers.jwt.privateKey = config.auth.providers.jwt.privateKey.replace('${APP_ROOT}', process.env.APP_ROOT);
-            config.auth.providers.jwt.publicKey = config.auth.providers.jwt.publicKey.replace('${APP_ROOT}', process.env.APP_ROOT);
+            if (config.db)
+                config.db.host = process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT;
+            if (config.auth && config.auth.providers && config.auth.providers.jwt) {
+                config.auth.providers.jwt.privateKey = config.auth.providers.jwt.privateKey.replace('${APP_ROOT}', process.env.APP_ROOT);
+                config.auth.providers.jwt.publicKey = config.auth.providers.jwt.publicKey.replace('${APP_ROOT}', process.env.APP_ROOT);
+            }
         }
         return config;
     }
