0a868fd379b63668099174d31fa97f976c0b9264
Denis Tatarenko <detat@ciklum.com>
2018-07-06 17:06:48 +0300

    --upd

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,20 @@
 import {Product} from '../models/product';
-import {CrudController} from "../core/crud.controller";
+import { CrudGameController } from './crud.game.controller';
 
-export class ProductController extends  CrudController {
+export class ProductController extends  CrudGameController {
    _activeRecordClass = Product;
+
+   public actionPost = (req: any, res: any, next: any):Promise<any> => {
+        /** @TODO: if not sandbox */
+        return Promise.resolve(
+            res.status(403).send('Can\'t be changed directly, only through the game actions')
+        );
+    }
+
+    public actionDelete = (req: any, res: any, next: any):Promise<any> => {
+        /** @TODO: if not sandbox */
+        return Promise.resolve(
+            res.status(403).send('Can\'t be changed directly, only through the game actions')
+        );
+    }
 }
--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,6 +1,20 @@
 import { Project } from '../models/project';
-import {CrudController} from "../core/crud.controller";
+import { CrudGameController } from './crud.game.controller';
 
-export class ProjectController extends  CrudController {
-   _activeRecordClass = Project;
+export class ProjectController extends  CrudGameController {
+    _activeRecordClass = Project;
+
+    public actionPost = (req: any, res: any, next: any):Promise<any> => {
+        /** @TODO: if not sandbox */
+        return Promise.resolve(
+            res.status(403).send('Can\'t be changed directly, only through the game actions')
+        );
+    }
+
+    public actionDelete = (req: any, res: any, next: any):Promise<any> => {
+        /** @TODO: if not sandbox */
+        return Promise.resolve(
+            res.status(403).send('Can\'t be changed directly, only through the game actions')
+        );
+    }
 }
